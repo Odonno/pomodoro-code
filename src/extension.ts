186af595f52211e362d856b17535d937a1944066
Odonno <bottiau.david@laposte.net>
2015-11-18 23:26:31 +0100

    feat(app): create a Pomodoro class to show time, default is '00:00'

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,53 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+// the module 'vscode' contains the VS Code extensibility API
+// import the module and reference it with the alias vscode in your code below
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode'; 
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pomodoro-code" is now active!'); 
+	console.log('Congratulations, your extension "pomodoro-code" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-		// The code you place here will be executed every time your command is executed
+	let pomodoro = new Pomodoro();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-	
-	context.subscriptions.push(disposable);
+	// Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(pomodoro);
+}
+
+class Pomodoro {
+	private _statusBarItem: StatusBarItem;
+	private _timer;
+
+	constructor() {
+		// create a new status bar item
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+		
+		this._timer = setInterval(() => this.update(), 1000);
+	}
+
+	public start() {
+
+	}
+
+	public stop() {
+
+	}
+
+	public reset() {
+
+	}
+
+	private update() {
+		// update the status bar
+		this._statusBarItem.text = '00:00';
+		this._statusBarItem.show();
+	}
+
+	dispose() {
+        this._statusBarItem.dispose();
+    }
 }
