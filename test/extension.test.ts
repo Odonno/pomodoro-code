7286b0c513e8754c46459a073c9e0c852c7279b1
Odonno <bottiau.david@laposte.net>
2015-11-22 18:25:12 +0100

    tests(pomodoro): resetting a working Pomodoro should stop Pomodoro

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -179,6 +179,19 @@ suite("Extension Tests", () => {
 			assert.equal(PomodoroStatus.None, pomodoro.status);
 			done();
 		});
+		
+		test("Resetting a working Pomodoro should stop Pomodoro", (done) => {
+			// arrange
+			let pomodoro = new Pomodoro();
+			pomodoro.start();
+			
+			// act
+			pomodoro.reset();
+			
+			// assert
+			assert.equal(PomodoroStatus.None, pomodoro.status);
+			done();
+		});
 	});
 
 });
